1cd6a47e6042f366270b492bf0f62ed654afc19d
Anatoliy <tolik_osmolovsky@mail.ru>
2026-06-15 21:58:55 +0300

    Проверка 8

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
 // Middleware для проверки JWT
